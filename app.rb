91b800235c198624613155b2ec460d5f8343c486
patrick-rush <patrick.a.rush@gmail.com>
2020-08-11 15:42:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Patrick."
+    end
+
+    get '/hometown' do 
+        "My hometown is Richmond, VA."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Clocks."
+    end
+
 end
