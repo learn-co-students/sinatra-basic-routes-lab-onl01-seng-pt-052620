a3ff066e10a819bb7519ab7eaaa48494667e38c7
Mitzi17 <m_estrada17@live.com>
2020-08-13 21:56:15 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do 
+    "My name is Mitzi!"
+end
+
+get '/hometown' do
+    "My hometown is Rowland"
+end
+
+get '/favorite-song' do
+    "My favorite song is Savage Love (lol)."
+end
+
 end
