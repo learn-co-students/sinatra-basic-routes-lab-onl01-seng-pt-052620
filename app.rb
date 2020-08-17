85783c8f3867067d2d2768c76d0575c40db586b8
M. Gage Morgan <gage@gages.blog>
2020-08-17 19:51:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Earl"
+  end
+
+  get '/hometown' do
+    "My hometown is Lima"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Come & Go by Marshmello, subject to change."
+  end
 end
