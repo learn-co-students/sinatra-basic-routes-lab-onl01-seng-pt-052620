5a7435d6671c37a0418e19c867b61fcd6507a1c5
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-08-24 13:20:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Yekaterina Petrovna Zamolodchikova, but your dad just calls me Katya."
+    end
+
+    get '/hometown' do
+        "My hometown is Hell"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is the one with that guy and it goes boom-pop-boom-pop-pop"
+    end
+
 end
